12d5cdcf3f21fd8742171eac9d1f6e3eed237a68
Abiya <aabbiiyyaa@gmail.com>
2023-10-01 00:34:58 +0700

    Edit server.py

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
 #TP SISTER MOD 3
 #ANGGOTA 1 : Muhammad Abiya Makruf - 1301213157 - MTA
 #ANGGOTA 2 : Galih Akbar Nugraha - 1301213060 - AFN
+
+#TCP Client
+import socket
--- server.py
+++ server.py
@@ -1,3 +1,11 @@
 #TP SISTER MOD 3
 #ANGGOTA 1 : Muhammad Abiya Makruf - 1301213157 - MTA
 #ANGGOTA 2 : Galih Akbar Nugraha - 1301213060 - AFN
+
+#TCP Server
+import socket
+TCP_IP = ''
+TCP_PORT = 5000
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+server.bind((TCP_IP, TCP_PORT))
+server.listen(2)
