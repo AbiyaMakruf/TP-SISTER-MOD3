d6a91ce4dc47f8533b8de79261c665cf551a0996
AbiyaMakruf <aabbiiyyaa@gmail.com>
2023-10-01 14:21:23 +0700

    Finsih server.py

--- server.py
+++ server.py
@@ -5,6 +5,7 @@
 #Import
 import socket
 import threading
+import datetime
 
 #Class Server
 class Server:
@@ -24,16 +25,41 @@ class Server:
         while True: #Selama tidak ada interupsi keyboard
             conn, addr = self.server.accept()
             self.clients.append(conn)
-            client = threading.Thread(target=self.handleClient, args=(conn))
+            client = threading.Thread(target=self.onReceiveMessage, args=(conn,addr,))
             client.start()
             print(addr, "berhasil terkoneksi")
+            print("Jumlah client saat ini: ", len(self.clients),end="\n\n")
 
-    def handleClient(self):
-        pass
+    def onReceiveMessage(self, conn,addr):
+        try:
+            while True:
+                message = conn.recv(1024).decode()
+                if not message:
+                    self.deleteClient(conn,addr)
+                    break
+                print(f"{datetime.datetime.now()} {message}")
 
+                self.sendMessage(message, conn)
+        except Exception as e:
+            print("An error occurred while receiving messages from a client:")
+            print(e)
 
+    def sendMessage(self, message, conn):
+        for client in self.clients:
+            if client != conn:
+                try:
+                    client.send(message.encode())
+                except Exception as e:
+                    print("An error occurred while forwarding message to a client:")
+                    print(e)
+        
+    def deleteClient(self, conn, addr):
+        self.clients.remove(conn)
+        print(addr, "terputus")
+        print("Jumlah client saat ini: ", len(self.clients),end="\n\n")
 
 #Start server
-#TODO host sama port mau dibikin input atau tidak?
-server = Server('192.168.18.25',5000)
+hostname = socket.gethostname()
+ipv4_address = socket.gethostbyname(hostname)
+server = Server(ipv4_address,5000)
 server.start()
